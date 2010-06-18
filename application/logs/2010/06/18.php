bd615fb15303912a058620080e506120e617f32d
Sam de Freyssinet <sam@def.reyssi.net>
2010-06-18 15:18:44 +0100

    Working injest

--- application/logs/2010/06/18.php
+++ application/logs/2010/06/18.php
@@ -8,3 +8,6 @@
 2010-06-18 15:09:56 --- ERROR: ErrorException [ 1 ]: Class 'mongo_connection' not found ~ APPPATH/classes/tweet.php [ 5 ]
 2010-06-18 15:10:43 --- ERROR: ErrorException [ 1 ]: Class 'mongo_connection' not found ~ APPPATH/classes/tweet.php [ 5 ]
 2010-06-18 15:10:55 --- ERROR: ErrorException [ 1 ]: Class 'Mongo' not found ~ APPPATH/classes/mongoconnection.php [ 9 ]
+2010-06-18 15:12:35 --- ERROR: ErrorException [ 1 ]: Class 'Mongo' not found ~ APPPATH/classes/mongoconnection.php [ 9 ]
+2010-06-18 15:16:59 --- ERROR: ErrorException [ 2 ]: MongoCollection::insert() expects parameter 1 to be an array or object ~ APPPATH/classes/tweet.php [ 6 ]
+2010-06-18 15:17:59 --- ERROR: Kohana_Request_Exception [ 0 ]: Kohana_Request::_external_execute unable to complete external request with message : Error fetching remote http://search.twitter.com/search.json?lang=en&amp;amp;phrase=%23mongouk [ status 0 ]  ~ SYSPATH/classes/kohana/request.php [ 1294 ]
