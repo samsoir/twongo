7f770153ac18306b537c51e87e809c09365fcf6a
Sam de Freyssinet <sam@def.reyssi.net>
2010-06-18 15:04:56 +0100

    Initial cron work

--- application/classes/controller/cron.php
+++ application/classes/controller/cron.php
@@ -1,5 +1,27 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-
+//http://search.twitter.com/search.json?lang=en&phrase=%23mongouk
 class Controller_Cron extends Controller {
-	
+
+	public static $twitter_endpoint = 'http://search.twitter.com/search.json?lang=en&phrase=%23mongouk';
+
+	public function __construct(Kohana_Request $request)
+	{
+		parent::__construct($request);
+
+		if (Kohana::$environment === Kohana::PRODUCTION and ! Kohana::$is_cli)
+		{
+			throw new Kohana_Exception('This can only be run in CLI mode');
+		}
+	}
+
+	public function action_injest()
+	{
+		$tweets = Request::factory(Controller_Cron::$twitter_endpoint)
+			->execute();
+		var_dump(json_decode($tweets->body));
+	}
+
+
 }
+
+
---
+++ application/logs/2010/06/18.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-06-18 15:00:49 --- ERROR: ErrorException [ 2048 ]: Creating default object from empty value ~ SYSPATH/classes/kohana/request.php [ 1117 ]
+2010-06-18 15:02:16 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI:  ~ SYSPATH/classes/kohana/request.php [ 729 ]
+2010-06-18 15:02:26 --- ERROR: ReflectionException [ 0 ]: Method action_injest does not exist ~ SYSPATH/classes/kohana/request.php [ 1091 ]
+2010-06-18 15:02:40 --- ERROR: ErrorException [ 8 ]: Undefined variable: twitter_endpoint ~ APPPATH/classes/controller/cron.php [ 19 ]
+2010-06-18 15:03:37 --- ERROR: Kohana_Request_Exception [ 0 ]: Kohana_Request::_external_execute unable to complete external request with message : Error fetching remote http://search.twitter.com/search.json?lang=en&amp;amp;phrase=%23mongouk [ status 0 ]  ~ SYSPATH/classes/kohana/request.php [ 1294 ]
